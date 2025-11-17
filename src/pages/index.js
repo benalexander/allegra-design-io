50f6da634a0b187023ff352c8bb6e5ce67a01e87
benalexander <ben.alexander@gmail.com>
2025-11-17 16:58:57 +0000

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,44 @@
-import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
-      Hello World.{" "}
-      <Link href="/about">
-        About
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Allegra Design - Coming Soon</title>
+      </Head>
+      <div
+        style={{
+          backgroundColor: "#343c3e",
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "20px",
+        }}
+      >
+        <img
+          src="/allegra-design-logo.png"
+          alt="Allegra Design Logo"
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+            marginBottom: "40px",
+          }}
+        />
+        <h1
+          style={{
+            color: "#ffffff",
+            fontSize: "24px",
+            fontWeight: "300",
+            letterSpacing: "2px",
+            margin: 0,
+            textTransform: "uppercase",
+          }}
+        >
+          Coming Soon
+        </h1>
+      </div>
+    </>
   );
 }
